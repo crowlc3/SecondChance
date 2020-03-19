d6dbd9f19aa2c7a695ce71c6b0e6c08901151d71
Gary Wang <38331932+gwang111@users.noreply.github.com>
2020-03-19 17:15:29 -0400

    files for link scanner. Will continue to modify

---
+++ listscanner/links.js
@@ -0,0 +1,41 @@
+var allLinks = [];
+var visibleLinks = [];
+
+// Display all visible links.
+function showLinks() {
+  var linksTable = document.getElementById('links');
+  while (linksTable.children.length > 1) {
+    linksTable.removeChild(linksTable.children[linksTable.children.length - 1])
+  }
+  for (var i = 0; i < visibleLinks.length; ++i) {
+    var row = document.createElement('tr');
+    var col1 = document.createElement('td');
+    col1.innerText = visibleLinks[i];
+    row.appendChild(col1);
+    linksTable.appendChild(row);
+  }
+}
+
+// Add links to allLinks and visibleLinks, sort and show them.  send_links.js is
+// injected into all frames of the active tab, so this listener may be called
+// multiple times.
+chrome.extension.onRequest.addListener(function(links) {
+  for (var index in links) {
+    allLinks.push(links[index]);
+  }
+  allLinks.sort();
+  visibleLinks = allLinks;
+  showLinks();
+});
+
+// Set up event handlers and inject send_links.js into all frames in the active
+// tab.
+window.onload = function() {
+  chrome.windows.getCurrent(function (currentWindow) {
+    chrome.tabs.query({active: true, windowId: currentWindow.id},
+                      function(activeTabs) {
+      chrome.tabs.executeScript(
+        activeTabs[0].id, {file: 'send_links.js', allFrames: true});
+    });
+  });
+};
