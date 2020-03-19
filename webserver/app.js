928d14e4422561fee9ce3009cd5a3c977a3ba984
ChristopherPence <chris1599@live.com>
2020-03-19 14:29:10 -0400

    Convert app to module

--- webserver/app.js
+++ webserver/app.js
@@ -1,16 +1,8 @@
-// Second Chance Webserver File
+// Second Chance Module File
 // SDD 2020
 
 require('dotenv').config();
-const express = require('express');
-const app = express();
 
-const HTTP_PORT = process.env.HTTP_PORT || "3000";
-
-app.get("/", (req, res) => {
-	res.status(200).send("Success. Application is running.");
-});
-
-app.listen(HTTP_PORT, () => {
-	console.log("Express application listening on port " + HTTP_PORT);
-});
+module.exports = function(url, callback){
+	callback('Hello World');
+}
