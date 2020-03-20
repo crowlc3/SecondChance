2869226a8e62bc0664d939320c50b59e45c5288e
OldManZhao <howardzhao99@aim.com>
2020-03-20 16:13:22 -0700

    Update catch.js

--- catch.js
+++ catch.js
@@ -1,20 +1,56 @@
-// JavaScript source code
-/*var cancel = function (details) {
-    console.log("please oh please i beg of you please print in the console i beg of you kind sir");
-};*/
-/*
+//https://stackoverflow.com/questions/41068295/electron-prevent-cancel-page-navigation
+//may need above for future reference for blocking off sites in database
+
+//need to filter out all background connections
+//ex. https://ssl.gstatic.com/docs/common/netcheck.gif* --> find way to get wildcards to work
+//maybe something like url.include("ssl.gstatic.com") === 0? --> may need import though
+
+var new_site = 1;
+var url = "";
+
+//we're running an O(N + M) solution here since we dont know the import
+function url_filter(url, check) {
+
+    var x;
+    for (x = 0; x < (url.length - check.length + 1); x++) {
+        console.log();
+        if (url[x] === check[0]) {
+            var y;
+            let cont = 1;
+            for (y = 0; y < check.length; y++) {
+                if (url[x + y] !== check[y]) {
+                    cont = 0;
+                    break;
+                }
+            }
+            if (cont === 1) {
+                //console.log("this is a match");
+                return true;
+            }
+        }
+    }
+   // console.log("not a match");
+    return false;
+
+}
+
 chrome.webRequest.onBeforeRequest.addListener(function (details) {
-    console.log("please oh please i beg of you please print in the console i beg of you kind sir");
-    chrome.tabs.sendMessage(details.tabId, { "message": details.url }, function (response) { });
-});
+    
+    console.log(details.url);
+    if (new_site && url !== details.url) {
 
-chrome.webRequest.onBeforeRedirect.addListener(function (details) {
-    console.log("This is a redirect");
-    chrome.tabs.sendMessage(details.tabId, { "message": details.url }, function (response) { });
-});*/
+        url = details.url;
 
-var callback = function (details) {
-    console.log("redirect caught!");
-};
+        /* this will check if the url contains a certain substring in the url
+         * if (url_filter(url, "ssl.gstatic")) {
+            console.log("echoing the skip");
+            return;
+        }*/
 
-chrome.webRequest.onBeforeRedirect.addListener(callback);
+        //silently block redirect from occuring
+        //return { redirectUrl: "javascript:" };
+    }
+
+},
+    {urls: ["<all_urls>"]
+}, ["blocking"]);
