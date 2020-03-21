5911870612cd8c611637e36c25c6ad92f2feea06
OldManZhao <howardzhao99@aim.com>
2020-03-20 17:44:42 -0700

    can grab main frame url now

--- catch.js
+++ catch.js
@@ -5,8 +5,8 @@
 //ex. https://ssl.gstatic.com/docs/common/netcheck.gif* --> find way to get wildcards to work
 //maybe something like url.include("ssl.gstatic.com") === 0? --> may need import though
 
-var new_site = 1;
 var url = "";
+var analysis_running = 0;
 
 //we're running an O(N + M) solution here since we dont know the import
 function url_filter(url, check) {
@@ -29,23 +29,31 @@ function url_filter(url, check) {
             }
         }
     }
-   // console.log("not a match");
+    //console.log("not a match");
     return false;
 
 }
 
 chrome.webRequest.onBeforeRequest.addListener(function (details) {
     
-    console.log(details.url);
-    if (new_site && url !== details.url) {
+    if (url !== details.url && details.type === "main_frame") {
 
         url = details.url;
+        console.log(url);
 
-        /* this will check if the url contains a certain substring in the url
-         * if (url_filter(url, "ssl.gstatic")) {
-            console.log("echoing the skip");
+        // this will check if the url contains a certain substring in the url
+        if (url_filter(url, "ssl.gstatic")) {
+            //console.log("echoing the skip");
             return;
-        }*/
+        }
+
+        /*
+        analysis_running = 1;
+        //call url analysis
+        if (analysis_running ) {
+
+        }
+        */
 
         //silently block redirect from occuring
         //return { redirectUrl: "javascript:" };
