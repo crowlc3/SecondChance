13db795c8a10dcce941d347773c1d55c3f771d14
ChristopherPence <chris1599@live.com>
2020-03-22 11:55:46 -0400

    Added url query to request

--- webserver/app.js
+++ webserver/app.js
@@ -1,6 +1,24 @@
 // Second Chance Module File
 // SDD 2020
 
+// This next section contains the portion of my webserver that handles the requests and directs them to this file
+/*
+// Second chance app for SDD
+app.get('/secondchance', function(req,res){
+  if(req.query.url == null){
+    res.status(400);
+    res.send("Invalid request.");
+  }
+  else{
+    secondchance.checkLink(req.query.url, function(url_status){
+      res.status(200);
+      res.send(url_status);
+    });
+  }
+});
+*/
+
+// This portion handles the redirected requests from my webserver
 exports.checkLink = function(url, callback){
-	callback('Hello World');
+	callback(url);
 }
