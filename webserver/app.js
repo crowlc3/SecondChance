7246a6e3cc23cbb0409cffdb75fce60ddffcf17b
ChristopherPence <chris1599@live.com>
2020-03-23 21:26:55 -0400

    Start of database interactions

--- webserver/app.js
+++ webserver/app.js
@@ -18,7 +18,105 @@ app.get('/secondchance', function(req,res){
 });
 */
 
+/*
+Responses to client should return JSON structure like such:
+
+{
+	success: true,
+	url: "google.com"
+	score: 0,
+	safe: true
+}
+
+*/
+
+// Pull in variables from the environment
+require('dotenv').config();
+
+// Connect to the database
+const { Client } = require('pg');
+const client = new Client({
+  host: process.env.SDD_DB_HOST,
+  port: process.env.SDD_DB_PORT,
+  user: process.env.SDD_DB_USER,
+  password: process.env.SDD_DB_PASS,
+  database: process.env.SDD_DB_DATA,
+  query_timeout: 2000
+});
+
+client.connect(err => {
+  if (err) {
+    console.error('connection error', err.stack)
+  } else {
+    console.log('connected')
+  }
+})
+
 // This portion handles the redirected requests from my webserver
 exports.checkLink = function(url, callback){
-	callback(url);
+	addToMaster(url, 0, true);
+
+	callback({
+		success: true,
+		url: url,
+		score: 0,
+		safe: true
+	});
+
+
+	// Virus Total
+	// Listen for error code 429
+
 }
+
+function makeAPICall(){
+
+}
+
+// Read the top most item from the queue
+function readQueue(){
+	client.query('SELECT url FROM queue ORDER BY date_added ASC LIMIT 1;', (err, res) => {
+		if(err){
+			console.log(err.stack);
+			return null;
+		}
+		else{
+			console.log(res.rows[0]);
+			return res.rows[0];
+		}
+	});
+}
+
+// Add a URL to the queue
+function addToQueue(url){
+	client.query('INSERT INTO queue (url) VALUES ($1) ON CONFLICT DO NOTHING;', [url], (err, res) => {
+		if(err){
+			console.log(err.stack);
+			return false;
+		}
+		else{
+			console.log(res);
+			return true;
+		}
+	});
+}
+
+// Get the score of a url in master
+function readMaster(){
+	
+}
+
+// Add a url to the master list
+function addToMaster(url, score, safe){
+	client.query('INSERT INTO master (url, score, safe) VALUES ($1, $2, $3) ON CONFLICT (url) DO UPDATE SET score = $2, safe = $3, date_added = CURRENT_DATE;', [url, score, safe], (err, res) => {
+		if(err){
+			console.log(err.stack);
+			return false;
+		}
+		else{
+			console.log(res);
+			return true;
+		}
+	});
+}
+
