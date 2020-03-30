5673f5b33a22247445f2972b5c13ceba12443183
crowlc3 <crowlc3@rpi.edu>
2020-03-30 13:31:12 -0400

    Progress on popup

--- popup.js
+++ popup.js
@@ -1,20 +1,28 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var checkPageButton = document.getElementById('checkPage');
-  checkPageButton.addEventListener('click', function() {
+$(function(){
+  $('#name').keyup(function(){
+    $('#greet').text('Hello ' + $('#name').val());
+  })
+})
 
-    chrome.tabs.getSelected(null, function(tab) {
-      d = document;
 
-      var f = d.createElement('form');
-      f.action = 'http://gtmetrix.com/analyze.html?bm';
-      f.method = 'post';
-      var i = d.createElement('input');
-      i.type = 'hidden';
-      i.name = 'url';
-      i.value = tab.url;
-      f.appendChild(i);
-      d.body.appendChild(f);
-      f.submit();
-    });
-  }, false);
-}, false);
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   var checkPageButton = document.getElementById('checkPage');
+//   checkPageButton.addEventListener('click', function() {
+
+//     chrome.tabs.getSelected(null, function(tab) {
+//       d = document;
+
+//       var f = d.createElement('form');
+//       f.action = 'http://gtmetrix.com/analyze.html?bm';
+//       f.method = 'post';
+//       var i = d.createElement('input');
+//       i.type = 'hidden';
+//       i.name = 'url';
+//       i.value = tab.url;
+//       f.appendChild(i);
+//       d.body.appendChild(f);
+//       f.submit();
+//     });
+//   }, false);
+// }, false);
