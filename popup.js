e36fef5dd72bb13964715b5797afda4a325df36b
crowlc3 <crowlc3@rpi.edu>
2020-04-06 13:22:17 -0400

    Whitelist Functionality
    A lot of stuff needs to be reworked and a lot of stuff is commented out however this is a step towards whitelist functionality. This returns the link entered

--- popup.js
+++ popup.js
@@ -1,8 +1,21 @@
-$(function(){
-  $('#name').keyup(function(){
-    $('#greet').text('Hello ' + $('#name').val());
-  })
-})
+// $(function(){
+//   $('#name').keyup(function(){
+//     $('#greet').text('Hello Bitch' + $('#name').val());
+//   })
+// });
+
+/*$(function(){
+	$('#whitelistlink').click(function(){
+		chrome.storage.sync.get('total',function(linky){
+			//var linkadd = $('#linkadd').val();
+			//$('#total').text(linkadd);
+			var l = $('#l').val();
+			chrome.storage.sync.set({'total': l});
+			$('#total').text(l);
+			$('#l').val('');
+		});
+	});
+});*/
 
 
 
@@ -26,3 +39,31 @@ $(function(){
 //     });
 //   }, false);
 // }, false);
+
+
+$(function(){
+
+    chrome.storage.sync.get(['total'],function(budget){
+        $('#total').text(budget.total);
+    });
+
+    $('#spendAmount').click(function(){
+        chrome.storage.sync.get(['total'],function(budget){
+            var newTotal = 0;
+            if (budget.total){
+                newTotal =budget.total;
+            }
+
+            var amount = $('#amount').val();
+            if (amount){
+                newTotal = amount;
+            }
+
+            $('#total').text(newTotal);
+            $('#amount').val('');
+
+           
+
+        });
+    });
+});
