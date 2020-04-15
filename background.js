51e728530d106e565f0adf43e98e9318dcdb605a
crowlc3 <crowlc3@rpi.edu>
2020-04-15 17:53:51 -0400

    testing

---
+++ background.js
@@ -0,0 +1,8 @@
+var win = window.open("", "Title", "toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=no,resizable=no,width=350,height=150,top="+(screen.height-800)+",left="+(screen.width-500));
+win.document.body.innerHTML =''+
+    '<body style = "background-color:red;">'+
+        '<p>We found this link to be __% Malicious</p>'+
+        '<p>Would You Like To Continue?</p>'+
+		'<button type = "button">Yes</button>'+
+		'<button type = "button">Nop</button>'+
+    '</body>';
--- test.js
+++ test.js
@@ -1,1 +1,18 @@
 console.log("Working");
+//window.open("index.html", "extension_popup", "width=300,height=400,status=no,scrollbars=yes,resizable=no");
+//window.open("chrome-extension://iblfmfpppnpafkgkgcdonpdpepjelcnf/_generated_background_page.html")
+
+
+/*var options = {
+	type: "basic",
+	title: "Test",
+	message: "work",
+	iconUrl:"icon48.png"
+};
+
+chrome.notifications.create(options, callback);
+
+
+function callback(){
+	console.log("WOOO");
+};*/
