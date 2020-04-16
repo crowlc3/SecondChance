afcf462bdadecc040adb7839a4cecc50dd139e5a
crowlc3 <crowlc3@rpi.edu>
2020-04-16 15:00:54 -0400

    some styling

--- popup.js
+++ popup.js
@@ -4,14 +4,14 @@ $(function(){
         $('#link').text(linkEnter.link);
 
 
-        var fs = require('fs')
+        /*var fs = require('fs')
         fs.appendFile('log.txt', 'new data', function (err) {
           if (err) {
             // append failed
           } else {
             // done
           }
-        })
+        })*/
 
     });
 
