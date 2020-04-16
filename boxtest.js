8a4afb7b5d16fe0af184b0111e357986084452ef
crowlc3 <crowlc3@rpi.edu>
2020-04-15 22:00:34 -0400

    uneccesary files

--- boxtest.js
+++
@@ -1,9 +0,0 @@
-function myFunction() {
-  var txt;
-  if (confirm("Press a button!")) {
-    txt = "You pressed OK!";
-  } else {
-    txt = "You pressed Cancel!";
-  }
-  document.getElementById("demo").innerHTML = txt;
-}
