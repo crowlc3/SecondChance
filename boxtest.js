8f558e834fa112f39b3ad3c65b131be1870df164
crowlc3 <crowlc3@rpi.edu>
2020-04-15 21:57:18 -0400

    popupboxes

---
+++ boxtest.js
@@ -0,0 +1,9 @@
+function myFunction() {
+  var txt;
+  if (confirm("Press a button!")) {
+    txt = "You pressed OK!";
+  } else {
+    txt = "You pressed Cancel!";
+  }
+  document.getElementById("demo").innerHTML = txt;
+}
