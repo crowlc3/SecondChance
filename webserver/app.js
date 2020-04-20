9387b0e7108873b9242084134346c6ca350510a9
ChristopherPence <chris1599@live.com>
2020-04-20 15:10:46 -0400

    demo changes

--- webserver/app.js
+++ webserver/app.js
@@ -54,6 +54,12 @@ exports.checkLink = function(url, callback){
 	});		
 }
 
+exports.updateLink = function(url, callback){
+	sanitizeURL(url, (cleanURL) => {
+		callback(updateMaster(cleanURL));
+	});
+}
+
 /*===============================================================================
 API CLASS
 ===============================================================================*/
@@ -194,6 +200,12 @@ function addToMaster(url, score, safe){
 		else{
 			return true;
 		}
+	});
+}
+
+function updateMaster(url){
+	client.query('UPDATE TABLE master SET safe = True WHERE url = $1', [url], (err, res) => {
+		return true;
 	});
 }
 
