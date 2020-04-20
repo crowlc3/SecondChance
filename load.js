cb45a5fdcc109ca485c2e5758cfb3be8b72f8b5f
crowlc3 <crowlc3@rpi.edu>
2020-04-20 14:46:13 -0400

    More Cleanup

--- load.js
+++
@@ -1,9 +0,0 @@
-function load() {
-    var links = "";
-    chrome.storage.local.get('link', function (result) {
-        links = result.links;
-        alert(result.links);
-        alert(JSON.stringify(result));
-        $("#link").val(links);
-    });
-}
