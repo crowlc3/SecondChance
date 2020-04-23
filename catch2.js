ecb3db0494f81362ce268437e2bd4cc910335178
OldManZhao <howardzhao99@aim.com>
2020-04-23 16:15:49 -0700

    linted

--- catch2.js
+++ catch2.js
@@ -1,10 +1,6 @@
 //https://stackoverflow.com/questions/41068295/electron-prevent-cancel-page-navigation
 //may need above for future reference for blocking off sites in database
 
-//need to filter out all background connections
-//ex. https://ssl.gstatic.com/docs/common/netcheck.gif* --> find way to get wildcards to work
-//maybe something like url.include("ssl.gstatic.com") === 0? --> may need import though
-
 var url = "https://chrispence.me";
 
 //we're running an O(N + M) solution here since we dont know the import
--- popup.js
+++ popup.js
@@ -2,27 +2,27 @@ $(function(){
 
     var list = [];
 
-    chrome.storage.local.get('link',function(linkEnter){
-        $('#link').text(linkEnter.link);
+    chrome.storage.local.get("link", function(linkEnter) {
+        $("#link").text(linkEnter.link);
     });
 
 
-    chrome.storage.local.get(null,function(returnback){
+    chrome.storage.local.get(null, function(returnback){
         console.log(returnback);
     });
 
-    $('#whiteList').click(function(){
-        chrome.storage.local.get(['link'],function(linkEnter){
-            var linky = $('#linky').val();
+    $("#whiteList").click(function() {
+        chrome.storage.local.get(["link"], function(linkEnter) {
+            var linky = $("#linky").val();
             var newLink = linky;
 
-            chrome.storage.local.set({'link': linky});
+            chrome.storage.local.set({"link": linky});
 
             fetch("https://chrispence.me/sddupdate?url=" + linky);
 
 
-            $('#link').text(newLink);
-            $('#linky').val('');
+            $("#link").text(newLink);
+            $("#linky").val("");
         });
     });
 });
